5726980e1db062fcbd54ed1322d972e6f9bb7fc2
Bryan Mohr <funkless@gmail.com>
2016-07-30 23:12:50 +0000

    Logic change for new circuit

--- pinTest.py
+++ pinTest.py
@@ -2,35 +2,20 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(18, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
 
 
 def pinChanged(channel):
 	if(GPIO.input(channel)):
-		print str(channel) + " closed"
+		print str(channel) + " circuit open!"
 	else:
-		print str(channel) + " open"
+		print str(channel) + " circuit closed"
 
 GPIO.add_event_detect(18, GPIO.BOTH, callback=pinChanged, bouncetime=200)
-GPIO.add_event_detect(23, GPIO.BOTH, callback=pinChanged, bouncetime=200)
-GPIO.add_event_detect(24, GPIO.BOTH, callback=pinChanged, bouncetime=200)
-
-if(GPIO.input(18)):
-	print "Door 1 is closed"
-else:
-	print "Door 1 is open"
-	
-if(GPIO.input(23)):
-	print "Door 2 is closed"
-else:
-	print "Door 2 is open"
-	
-if(GPIO.input(24)):
-	print "Door 3 is closed"
-else:
-	print "Door 3 is open"
+GPIO.add_event_detect(17, GPIO.BOTH, callback=pinChanged, bouncetime=200)
+GPIO.add_event_detect(25, GPIO.BOTH, callback=pinChanged, bouncetime=200)
 
 
 	
